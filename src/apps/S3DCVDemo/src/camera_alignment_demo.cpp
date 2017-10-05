75c20f9faaea51d281afb101c016a524f9d56c41
Hugo Bedard <hugobdard@gmail.com>
2017-10-05 17:32:01 -0400

    Template histogram on type

--- src/apps/S3DCVDemo/src/camera_alignment_demo.cpp
+++ src/apps/S3DCVDemo/src/camera_alignment_demo.cpp
@@ -140,7 +140,7 @@ int main(int argc, char* argv[]) {
   s3d::compute_disparities(pts1, pts2, back_inserter(disparities));
 
   // disparity histogram
-  auto hist = s3d::Histogram::Compute(disparities, 10);
+  auto hist = s3d::Histogram<double>::Compute(disparities, 10);
 
   double minDisp, maxDisp;
   std::tie(minDisp, maxDisp) = s3d::disparity_range(disparities);
--- src/core/s3d/test/utilities/tests_histogram.cpp
+++ src/core/s3d/test/utilities/tests_histogram.cpp
@@ -10,30 +10,30 @@ TEST(histogram, min) {
   std::vector<double> v;
   v.resize(10);
   std::iota(std::begin(v), std::end(v), 0.0);
-  EXPECT_DOUBLE_EQ(Histogram::Min(v), 0.0);
+  EXPECT_DOUBLE_EQ(Histogram<double>::Min(v), 0.0);
 }
 
 TEST(histogram, max) {
   std::vector<double> v;
   v.resize(10);
   std::iota(std::begin(v), std::end(v), 0.0);
-  EXPECT_DOUBLE_EQ(Histogram::Max(v), 9.0);
+  EXPECT_DOUBLE_EQ(Histogram<double>::Max(v), 9.0);
 }
 
 TEST(histogram, bin_size_trivial) {
-  Histogram hist;
+  Histogram<double> hist;
   constexpr double min = 0.0;
   constexpr double max = 10.0;
   constexpr double nbBins = 10.0;
-  EXPECT_DOUBLE_EQ(Histogram::BinSize(min, max, nbBins), 1.0);
+  EXPECT_DOUBLE_EQ(Histogram<double>::BinSize(min, max, nbBins), 1.0);
 }
 
 TEST(histogram, bin_size_less_trivial) {
-  Histogram hist;
+  Histogram<double> hist;
   constexpr double min = -15.0;
   constexpr double max = 5.5;
   constexpr double nbBins = 5.0;
-  EXPECT_NEAR(Histogram::BinSize(min, max, nbBins), 4.09, 0.01);
+  EXPECT_NEAR(Histogram<double>::BinSize(min, max, nbBins), 4.09, 0.01);
 }
 
 TEST(histogram, bin_index) {
@@ -43,19 +43,19 @@ TEST(histogram, bin_index) {
   auto expected = std::vector<int>{1, 3, 1, 1};
 
   for (int i = 0; i < value.size(); ++i) {
-    EXPECT_DOUBLE_EQ(Histogram::BinIdx(value[i], min[i], binSize[i]), expected[i]);
+    EXPECT_DOUBLE_EQ(Histogram<double>::BinIdx(value[i], min[i], binSize[i]), expected[i]);
   }
 }
 
 TEST(histogram, bin_index_max_value_no_overflow) {
   auto values = std::vector<double>{0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
-  auto hist = Histogram::Compute(values, 5);
+  auto hist = Histogram<double>::Compute(values, 5);
   EXPECT_GT(hist.binSize, 1.0 + 0.000000001);
 }
 
 TEST(histogram, compute) {
   auto values = std::vector<double>{1.5, 2.5, 4.6, 7.3, 8.9, 11.4};
-  auto hist = Histogram::Compute(values, 3);
+  auto hist = Histogram<double>::Compute(values, 3);
   EXPECT_EQ(hist.counts.size(), 3);
   EXPECT_EQ(hist.counts[0], 3);
   EXPECT_EQ(hist.counts[1], 1);
